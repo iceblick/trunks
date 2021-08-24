e1eb6acd8e56058086d5130533e71a9ecf876e71
iceblick <ruben.vanegas@cwc.com>
2021-08-24 18:28:27 -0500

    Ahi vamos commit

--- trunks/views.py
+++ trunks/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Trunk_OTN
+from .models import Trunk_DWDM
+from .models import Trunk_SDH
 
 # Create your views here.
+
+def trunk_list(request):
+    otn = Trunk_OTN.objects.filter(Published_date__lte=timezone.now()).order_by('Published_date')
+    dwdm = Trunk_DWDM.objects.filter(Published_date__lte=timezone.now()).order_by('Published_date')
+    sdh = Trunk_SDH.objects.filter(Published_date__lte=timezone.now()).order_by('Published_date')
+    return render(request, 'trunks/trunk_list.html', {'otn': otn, 'dwdm': dwdm, 'sdh': sdh})
